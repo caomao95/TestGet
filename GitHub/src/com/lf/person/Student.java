fc4d078ab92fdff8b86bb6872cc381aca7c624ae
caomao95 <528637588@qq.com>
2017-05-07 21:35:22 +0800

    test

---
+++ GitHub/src/com/lf/person/Student.java
@@ -0,0 +1,11 @@
+package com.lf.person;
+
+public class Student {
+	private String name;
+	private int age;
+	
+	public static void main(String[] args) {
+		Student s = new Student();
+		System.out.println("");
+	}
+}
